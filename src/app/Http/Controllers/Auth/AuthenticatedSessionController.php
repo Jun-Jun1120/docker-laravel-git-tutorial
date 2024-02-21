2cca80518c66dd8c720d4ea5ef524706ac286988
Jun-Jun1120 <j.yamazaki@hajimari.inc>
2024-02-21 17:46:00 +0900

    feature/'feature/認証機能'にて追加し忘れていたファイル

---
+++ src/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Display the login view.
+     */
+    public function create(): View
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): RedirectResponse
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        // Update the redirect to the custom URL after login
+        return redirect('http://localhost:8080/toppage');
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        // Update the redirect to the custom URL after logout
+        return redirect('http://localhost:8080/toppage');
+    }
+}
