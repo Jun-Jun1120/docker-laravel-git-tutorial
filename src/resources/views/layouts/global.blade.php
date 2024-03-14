b382a57011857f18a6800e50d0250a6bd29d8e46
Jun-Jun1120 <j.yamazaki@hajimari.inc>
2024-03-14 18:18:02 +0900

    feat:「こんなときどうすればいい？」の作成

--- src/resources/views/layouts/global.blade.php
+++ src/resources/views/layouts/global.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Gitチュートリアルサイト')</title>
-    <link rel="stylesheet" href="/css/global.css">
+    <!-- Js -->
+    @vite('resources/js/app.js')
+    <!-- Css -->
+    <link href="{{ asset('css/global.css') }}" rel="stylesheet">
 </head>
+
 <body>
 <header>
     <div class="header-content">
-        <h1>Gitチュートリアルへようこそ</h1>
+        <h1>Git入門へようこそ</h1>
         <img src="/images/GitHub icon.png" alt="GitHub Icon" class="github-icon">
         @include('partials.auth-links')
     </div>
